8b8b2382b6a44264fc2bb312c6291c40afe3038d
MS AYYANAR <senthilayyanar2018@gmail.com>
2025-09-02 00:31:05 +0530

    Active tab changes

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,40 +1,49 @@
 <template>
     <nav class="nav-bar">
         <ul class="nav-tabs">
-            <li class="page-name active">Home</li>
-            <li class="page-name">About</li>
-            <li class="page-name">Skills</li>
-            <li class="page-name">Achievements</li>
+            <li v-for="page in  pages " :key="page" :class="{ active: currentTab === page, 'page-name': true }"
+                @click="currentTab = page">
+                {{ page }}
+            </li>
         </ul>
     </nav>
 </template>
 
 <script setup>
-    // const pages = ref(["Home", "About", "Skills", "Achievements"]);
+import { ref } from 'vue';
+const currentTab = ref("Home");
+const pages = ref(["Home", "About", "Skills", "Achievements"]);
+
 </script>
 
 <style>
-    .nav-bar{
-        background-color: #890eec;
-        color: white;
-        font-size: 18px;
-    }
+.nav-bar {
+    background-color: #890eec;
+    color: white;
+    font-size: 18px;
+}
 
-    .nav-tabs{
-        display: flex;
-        justify-content: flex-end;
-        align-items: center;
-        gap: 30px;
-        height: 50px;
-    }
+.nav-tabs {
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    gap: 20px;
+    height: 50px;
+}
 
-    .nav-tabs .page-name{
-        list-style: none;
-        cursor: pointer;
-    }
+.nav-tabs .page-name {
+    list-style: none;
+    cursor: pointer;
+    caret-color: transparent;
+    margin-right: 20px;
+}
 
-    .page-name.active{
-        background-color: #c694ee;
-        padding: 5px;
-    }
+.page-name.active {
+    background-color: #ddb4ff;
+    padding: 5px 15px;
+    border-radius: 5px;
+    color: rgb(29, 29, 29);
+    transition: background-color 0.5s;
+    font-weight: 600;
+}
 </style>
