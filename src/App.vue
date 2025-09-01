ed838cd33d6ac10ce95596658173df67ed3066d0
AYYANAR <senthilayyanar2018@gmail.com>
2025-09-01 19:52:00 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import MainPage from './components/MainPage.vue';
+</script>
+
+<template>
+  <MainPage></MainPage>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/MainPage.vue
@@ -0,0 +1,23 @@
+<template>
+    <nav class="nav-bar">
+        <ul class="tabs">
+            <li class="page-name active">Home</li>
+            <li class="page-name">About</li>
+            <li class="page-name">Skills</li>
+            <li class="page-name">Achievements</li>
+        </ul>
+    </nav>
+</template>
+
+<script setup>
+    // const pages = ref(["Home", "About", "Skills", "Achievements"]);
+</script>
+
+<style>
+    .nav-bar{
+        background-color: #890eec;
+        color: white;
+        font-weight: bold;
+        font-size: 18px;
+    }    
+</style>
