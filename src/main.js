1ec14a9e5737e1874ef93302c1190b80b84a5faa
MS AYYANAR <senthilayyanar2018@gmail.com>
2026-02-08 17:16:57 +0530

    Fixes

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue';
 import router from './routes/router';
+console.log("Worked!");
 
 createApp(App).use(router).mount('#app')
